9dec93c0f9aa079545c4ca55f876772ce3f4acdf
WendaLi <lwd19861127@hotmail.com>
2020-04-20 11:26:59 +0800

    Assignment2

---
+++ AutoLayoutStarter2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutStarter2
+//
+//  Created by WendaLi on 2020-04-20.
+//  Copyright © 2020 WendaLi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
